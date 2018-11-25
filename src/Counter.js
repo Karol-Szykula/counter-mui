f68bd21fc794099abc28e04f47b88138dbcc0b68
Karol-Szykula <karol.szykula@wp.pl>
2018-11-25 16:22:17 +0100

    Create empty component counter

---
+++ src/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Counter extends React.Component {
+    // constructor(props) {
+    //     super(props)
+    // }
+
+
+    render() {
+
+        return (
+            <div>
+                Counter
+            </div>
+        )
+    }
+}
+
+export default Counter
